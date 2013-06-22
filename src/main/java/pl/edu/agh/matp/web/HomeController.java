715c153ed2ba84479606b48775f380c7d661bf9e
mjamroz <mjamroz90@gmail.com>
2013-06-22 12:30:14 +0200

    Napisany widok + javascript + stub kodu serwerowego.

--- src/main/java/pl/edu/agh/matp/web/HomeController.java
+++ src/main/java/pl/edu/agh/matp/web/HomeController.java
@@ -3,6 +3,10 @@ package pl.edu.agh.matp.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * Created with IntelliJ IDEA.
@@ -19,6 +23,12 @@ public class HomeController {
     @RequestMapping(method = RequestMethod.GET)
     public String home(){
         return "home";
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.GET, produces = "application/json")
+    @ResponseBody
+    public List<String> search(@RequestParam("search_val")String searchVal){
+        return null;
     }
 
 }
